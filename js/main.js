c873fef8dca7dc16f4012ff9f837754a027ca629
ApcarJo <hansokaz@gmail.com>
2021-05-09 01:08:40 +0200

    Added while loop to erase previous input string

--- js/main.js
+++ js/main.js
@@ -14,31 +14,33 @@ console.log(arrayName[4]);
 var elms = document.getElementsByClassName("square");
 var n = elms.length;
 let i=0;
+var j=0;
 
 
-function changeColor() {
-        for(var i = 0; i < ite; i ++) { //Implementar do while para borrar el resto de la cadena
-        (function (i) {
-            setTimeout(function () {
-                elms[i].innerHTML = arrayName[i];
-                // elms[i].style.backgroundColor = white;
-            }, 100*i);
-          })(i);
-          
-        }
-}
+// function changeColor() {
+//         for(var i = 0; i <=ite; i ++) { 
+//         (function (i) {
+//             setTimeout(function () { 
+//                 elms[i].innerHTML = arrayName[i];
+//             }, 100*i);
+//           })(i);
+//         }      
+// }
 
 function sub(){
     product = document.getElementsByName("nombre")[0].value;
     let arrayName2 = Array.from(product);
 
-    for(var i = 0; i < product.length; i ++) {
+    for(var i = 0; i < product.length; i ++) { //Implementar do while para borrar el resto de la cadena
         (function (i) {
             setTimeout(function () {
                 elms[i].innerHTML = arrayName2[i];
-                elms[i].style.backgroundColor = color;
             }, 100*i);
           })(i);
+        }
+        while (i<n){
+            elms[i].innerHTML = "";
+            i++
         }
   };
 
