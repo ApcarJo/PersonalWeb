0cfa6cf2b533db91c5e33b58c849f174a8ff4472
ApcarJo <hansokaz@gmail.com>
2021-05-09 12:57:28 +0200

    New design synced with all the html

--- js/main.js
+++ js/main.js
@@ -7,48 +7,44 @@ console.log(`Hola ${nombre}, tu nombre tiene ${length} letras`);
 
 let arrayName = Array.from(nombre);
 
-console.log(arrayName);
-console.log(arrayName[0]);
-console.log(arrayName[4]);
-
-var elms = document.getElementsByClassName("square");
-var n = elms.length;
+var square = document.getElementsByClassName("square");
+var n = square.length;
 let i=0;
 var j=0;
-
-
-// function changeColor() {
-//         for(var i = 0; i <=ite; i ++) { 
-//         (function (i) {
-//             setTimeout(function () { 
-//                 elms[i].innerHTML = arrayName[i];
-//             }, 100*i);
-//           })(i);
-//         }      
-// }
 
 function sub(){
     product = document.getElementsByName("nombre")[0].value;
     let arrayName2 = Array.from(product);
 
-    for(var i = 0; i < product.length; i ++) { //Implementar do while para borrar el resto de la cadena
+    for(var i = 0; i < arrayName2.length; i ++) { //Implementar do while para borrar el resto de la cadena -- DONE
         (function (i) {
             setTimeout(function () {
-                elms[i].innerHTML = arrayName2[i];
+                square[i].innerHTML = arrayName2[i];
             }, 100*i);
           })(i);
-        }
-        while (i<n){
-            elms[i].innerHTML = "";
-            i++
-        }
-  };
-
-for(var w = 0; i < n; w ++) {
-    elms[w].onclick = function() {
-        changeColor();
+    }
+    while (i<n){
+            square[i].innerHTML = "";
+            i++;
     };
-    // elms[w].onmouseout = function() {
-    //     changeColor();
-    // };
-}
+};
+
+// product = document.getElementsById("nombre")[0].value;
+// let arrayName2 = Array.from(product);
+
+//     product.addEventListener("keydown", (e) {
+//         if (e.keyCode === 13) {
+
+//             for(var i = 0; i < arrayName2.length; i ++) {
+//                 (function (i) {
+//                     setTimeout(function () {
+//                         square[i].innerHTML = arrayName2[i];
+//                     }, 100*i);
+//                 })(i);
+//             }
+//             while (i<n){
+//                     square[i].innerHTML = "";
+//                     i++;
+//             };
+//         }
+//     });
